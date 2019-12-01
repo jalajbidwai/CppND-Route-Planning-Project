7749317c776d4cec3eda8c082fed8cbe3ebaf668
jalajbidwai <42255551+jalajbidwai@users.noreply.github.com>
2019-11-30 21:59:25 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,16 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+	float start_x, start_y, end_x, end_y;
+	std::cout << "Enter starting x coordinated between 0 - 99 : ";
+	std::cin >> start_x;
+	std::cout << "Enter starting y coordinated between 0 - 99 : ";
+	std::cin >> start_y;
+	std::cout << "Enter end x coordinated between 0 - 99 :";
+	std::cin >> end_x;
+	std::cout << "Enter end y coordinated between 0 - 99 :";
+	std::cin >> end_y;
+
 
     // Build Model.
     RouteModel model{osm_data};
